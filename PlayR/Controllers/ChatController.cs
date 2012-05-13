5b332a8c2eeb5fd0b9f6c8efabe78eba18cd54f2
Alex Ullrich <alex.ullrich@gmail.com>
2012-05-13 12:13:43 -0400

    initial commit

---
+++ PlayR/Controllers/ChatController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace PlayR.Controllers
+{
+    public class ChatController : Controller
+    {
+        //
+        // GET: /Chat/
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ PlayR/Hubs/Chat.cs
@@ -0,0 +1,13 @@
+﻿using SignalR.Hubs;
+
+namespace PlayR.Hubs
+{
+    public class Chat : Hub
+    {
+        public void Send(string message, string user)
+        {
+            // Call the addMessage method on all clients
+            Clients.addMessage(message, user);
+        }
+    }
+}
